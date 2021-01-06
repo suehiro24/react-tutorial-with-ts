6010c8c1882d43b087a738928d423a4377ec5072
HayataSato <hayatasato.0@gmail.com>
2021-01-06 10:25:25 +0900

    Stateのリフトアップ

--- src/index.tsx
+++ src/index.tsx
@@ -2,39 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+type squareType = string | null;
 
 interface SquareProps {
-    value: number;
+    value: squareType;
+    onClick: () => void;
 }
 
-interface SquareState {
-    value: string | null;
-}
-
-class Square extends React.Component<SquareProps, SquareState> {
-  constructor(props: SquareProps) {
-    super(props);
-    this.state = {
-        value: null,
-    };
-  }
+class Square extends React.Component<SquareProps> {
 
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: "X"})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
+interface BoardState {
+  squares: squareType[];
+}
 
-class Board extends React.Component {
+class Board extends React.Component<any, BoardState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i: number) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({squares: squares})
+  }
+
   renderSquare(i: number) {
-    return <Square value={i} />;
+    return (
+    <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
+    );
   }
 
   render() {
